d536dd2fd1103396420fe244df7acd20422ed8b1
porter oleson <porter.oleson@gmail.com>
2022-10-15 15:46:02 -0600

    Readme.md

---
+++ README.md
@@ -0,0 +1,36 @@
+# Overview
+
+{Important!  Do not say in this section that this is college assignment.  Talk about what you are trying to accomplish as a software engineer to further your learning.}
+
+{Provide a description the software that you wrote and how it integrates with a Cloud Database.  Describe how to use your program.}
+
+{Describe your purpose for writing this software.}
+
+{Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the software running, a walkthrough of the code, and a view of the cloud database.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Cloud Database
+
+I am using Firebase from Google Firestore as my cloud Database.
+
+My database has two tables, one for canivorous dinosaurs and one for herbivore dinosaurs. Inside those tables are the names of the dinosaurs and those tables hold the length in feet and the weight in tons of each dinosaur.
+
+# Development Environment
+
+I am using Visual Studio Code and Firebase. 
+
+I am using python 3.10 and I downloaded a module called firebase_admin that allows me to connect to the database in my program.
+
+# Useful Websites
+
+* [Firebase](https://firebase.google.com/docs)
+* [W3 Schools](https://www.w3schools.com/)
+* [Google Cloud] (https://cloud.google.com/docs)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+* I plan to add more facts to each of the dinosaurs such as time period and perhaps including a picture for each one.
+* I need to fix the program so that it has more error catching and stuff like that.
+* The last thing is that I want to add user authentication to it so that you can login and save your favorite dinosaurs.
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def initialize_firestore():
     # Setup Google Cloud Key - The json file is obtained by going to 
     # Project Settings, Service Accounts, Create Service Account, and then
     # Generate New Private Key
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]  = "inventory-d5804-firebase-adminsdk-2puf5-215941e1ae.json"
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]  = "porter-planner-firebase-adminsdk-wvwwg-b3c970100b.json"
 
     # Use the application default credentials.  The projectID is obtianed 
     # by going to Project Settings and then General.
@@ -22,113 +22,103 @@ def initialize_firestore():
     db = firestore.client()
     return db
 
-def add_new_item(db):
+def add_new_dino(db):
     '''
     Prompt the user for a new item to add to the inventory database.  The
     item name must be unique (firestore document id).  
     '''
+    while True:
+        collection = input("Herbivore or Carnivore: ")
+        if collection == "Herbivore" or collection == "Carnivore":
+            break
 
-    name = input("Item Name: ")
-    price = float(input("Price: "))
-    popular = input("Is it popular (Y/N): ") in ['Y','y']
-    qty = int(input("Initial Quantity: "))
+        print(f"\n{collection} is not an option, please try again.")
 
-    # Check for an already existing item by the same name.
+    name = input("Dinosaur: ")
+
+    try:
+        size = float(input("New Size (feet): "))
+        weight = float(input("New Weight (tons): "))
+    except:
+        print("You need to type in a number.")
+        main()
+
+    # Check for an already existing Dino by the same name.
     # The document ID must be unique in Firestore.
-    result = db.collection("inventory").document(name).get()
+    result = db.collection(collection).document(name).get()
     if result.exists:
         print("Item already exists.")
         return
 
     # Build a dictionary to hold the contents of the firestore document.
-    data = {"price" : price, 
-            "popular" : popular,
-            "qty" : qty}
-    db.collection("inventory").document(name).set(data) 
+    data = {"Size" : size,
+            "Weight" : weight}
+    db.collection(collection).document(name).set(data) 
 
-    # Save this in the log collection in Firestore       
-    log_transaction(db, f"Added {name} with initial quantity {qty}")
-
-def add_inventory(db):
+def update_dino(db):
     '''
     Prompt the user to add quantity to an already existing item in the
     inventory database.  
     '''
+    while True:
+        collection = input("Herbivore or Carnivore: ")
+        if collection == "Herbivore" or collection == "Carnivore":
+            break
 
-    name = input("Item Name: ")
-    add_qty = int(input("Add Quantity: "))
+        print(f"\n{collection} is not an option, please try again.")
 
-    # Check for an already existing item by the same name.
+    name = input("Dinosaur: ")
+
+    try:
+        new_size = float(input("New Size (feet): "))
+        new_weight = float(input("New Weight (tons): "))
+    except:
+        print("You need to type in a number.")
+
+    # Check for an already existing Dino by the same name.
     # The document ID must be unique in Firestore.
-    result = db.collection("inventory").document(name).get()
+    result = db.collection(collection).document(name).get()
     if not result.exists:
-        print("Invalid Item Name")
+        print("Invalid Dinosaur")
         return
 
     # Convert data read from the firestore document to a dictionary
     data = result.to_dict()
 
-    # Update the dictionary with the new quanity and then save the 
+    # Update the dictionary with the new size and weight and then save the 
     # updated dictionary to Firestore.
-    data["qty"] += add_qty
-    db.collection("inventory").document(name).set(data)
+    data["Size"] = new_size
+    data["Weight"] = new_weight
+    db.collection(collection).document(name).set(data)
 
-    # Save this in the log collection in Firestore
-    log_transaction(db, f"Added {add_qty} {name}")
-
-def use_inventory(db):
-    '''
-    Prompt the user to use quantity from an already existing item in the
-    inventory database.  An error will be given if the requested amount
-    exceeds the quanity in the database.
-    '''
-
-    name = input("Item Name: ")
-    use_qty = int(input("Use Quantity: "))
-
-    # Check for an already existing item by the same name.
-    # The document ID must be unique in Firestore.
-    result = db.collection("inventory").document(name).get()
-    if not result.exists:
-        print("Invalid Item Name")
-        return
-
-    # Convert data read from the firestore document to a dictionary
-    data = result.to_dict()
-
-    # Check for sufficient quantity.
-    if use_qty > data["qty"]:
-        print(f"Not enough inventory. Only {data['qty']} left.")
-        return
-
-    # Update the dictionary with the new quanity and then save the 
-    # updated dictionary to Firestore.
-    data["qty"] -= use_qty
-    db.collection("inventory").document(name).set(data)
-
-    # Save this in the log collection in Firestore
-    log_transaction(db, f"Used {use_qty} {name}")
-
-def search_inventory(db):
+def search_dinos(db):
     '''
     Search the database in multiple ways.
     '''
 
     print("Select Query")
-    print("1) Show All Inventory")        
-    print("2) Show Unstocked Inventory")
-    print("3) Show Popular Inventory with Low Inventory")
+    print("1) Show All Dinosaurs")        
+    print("2) Show Big Dinosaurs")
+    print("3) Show Small Dinosaurs")
     choice = input("> ")
+    while True:
+        collection = input("Herbivore or Carnivore: ")
+        if collection == "Herbivore" or collection == "Carnivore":
+            break
+
+        print(f"\n{collection} is not an option, please try again.")
+
     print()
 
-    # Build and execute the query based on the request made
+    #     results = db.collection(collection).where("popular","==",True). \
+    #                                          where("qty","<=", 5).get()
+
     if choice == "1":
-        results = db.collection("inventory").get()
+        results = db.collection(collection).get()
     elif choice == "2":
-        results = db.collection("inventory").where("qty","==",0).get()
+        results = db.collection(collection).where("Weight",">=",2).get()
     elif choice == "3":
-        results = db.collection("inventory").where("popular","==",True). \
-                                             where("qty","<=", 5).get()
+        results = db.collection(collection).where("Weight", "<",2).get()
     else:
         print("Invalid Selection")
         return
@@ -136,66 +126,43 @@ def search_inventory(db):
     # Display all the results from any of the queries
     print("")
     print("Search Results")
-    print(f"{'Name':<20}  {'Price':<10}  {'Popular':<10}  {'Qty':<10}")
+    print(f"{'Name':<20}  {'Size (feet)':<10}  {'Weight (tons)':<10}")
     for result in results:
         item = result.to_dict()
-        print(f"{result.id:<20}  {item['price']:<10}  {str(item['popular']):<10}  {item['qty']:<10}")
-    print()    
+        print(f"{result.id:<20}  {item['Size']:<10}  {item['Weight']:<10}")
+    print()   
 
-def log_transaction(db, message):
-    '''
-    Save a message with current timestamp to the log collection in the
-    Firestore database.
-    '''
-    data = {"message" : message, "timestamp" : firestore.SERVER_TIMESTAMP}
-    db.collection("log").add(data)    
+def delete_dino(db):
+    while True:
+        collection = input("Herbivore or Carnivore: ")
+        if collection == "Herbivore" or collection == "Carnivore":
+            break
 
-def notify_stock_alert(results, changes, read_time):
-    '''
-    If the query of out of stock items changes, then display the changes.
-    ADDED = New out of stock item added to the list since registration
-    MODIFIED = An out of stock item was modified but still out of stock
-    REMOVED = An out of stock item is no longer out of stock
-    '''
+        print(f"\n{collection} is not an option, please try again.")
 
-    for change in changes:
-        if change.type.name == "ADDED": 
-            print()
-            print(f"OUT OF STOCK ALERT!! ORDER MORE: {change.document.id}")
-            print()
-        elif change.type.name == "REMOVED":
-            print()
-            print(f"ITEM HAS BEEN RE-STOCKED!! READY TO USE: {change.document.id}")
-            print()
-    
-def register_out_of_stock(db):
-    '''
-    Request a query to be monitored.  If the query changes, then the
-    notify_stock_alert will be called.
-    '''
-    db.collection("inventory").where("qty","==",0).on_snapshot(notify_stock_alert)
+    name = input("Dinosaur to be deleted: ")
+    db.collection(collection).document(name).delete()
 
 def main():
     db = initialize_firestore()
-    register_out_of_stock(db)
     choice = None
     while choice != "0":
         print()
         print("0) Exit")
-        print("1) Add New Item")
-        print("2) Add Quantity")
-        print("3) Use Quantity")
-        print("4) Search Inventory")
+        print("1) Add New Dinosaur")
+        print("2) Update Dinosaur")
+        print("3) Search Dinosaurs")
+        print("4) Delete Dinosaur")
         choice = input(f"> ")
         print()
         if choice == "1":
-            add_new_item(db)
+            add_new_dino(db)
         elif choice == "2":
-            add_inventory(db)
+            update_dino(db)
         elif choice == "3":
-            use_inventory(db)
+            search_dinos(db)       
         elif choice == "4":
-            search_inventory(db)                        
+            delete_dino(db)                     
 
 if __name__ == "__main__":
     main()
